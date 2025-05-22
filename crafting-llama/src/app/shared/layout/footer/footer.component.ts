259c7073248c3144490a72b423881362e5e68348
Michael Wilcome <mwilcomesc@gmail.com>
2025-05-21 22:04:39 -0400

    Front Page Rework

--- crafting-llama/src/app/shared/layout/footer/footer.component.ts
+++ crafting-llama/src/app/shared/layout/footer/footer.component.ts
@@ -1,15 +1,25 @@
-import { Component, signal } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-footer',
-    standalone: true,
-    templateUrl: './footer.component.html',
-    imports: [
-        RouterLink
-    ],
-    styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-    readonly year = signal(new Date().getFullYear());
+  isLlamaVisible = false;
+  isWiggling = false;
+
+  onMouseEnter(): void {
+    this.isLlamaVisible = true;
+    this.isWiggling = true;
+    setTimeout(() => {
+      this.isWiggling = false;
+    }, 2000);
+  }
+
+  onMouseLeave(): void {
+    this.isLlamaVisible = false;
+    this.isWiggling = false;
+  }
 }
