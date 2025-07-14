7d5aef438735a808998adb7188153e25a1d2e252
Michael Wilcome <mwilcomesc@gmail.com>
2025-07-14 16:06:04 -0400

    Footer Styling Updated

--- src/app/features/custom-order/price-disclaimer/price-disclaimer.component.ts
+++ src/app/features/custom-order/price-disclaimer/price-disclaimer.component.ts
@@ -1,11 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { NgIf } from '@angular/common';
 import { PriceDisclaimerService } from './price-disclaimer.service';
 
 @Component({
     selector: 'app-price-disclaimer',
     standalone: true,
-    imports: [NgIf],
     templateUrl: './price-disclaimer.component.html',
     styleUrls: ['./price-disclaimer.component.scss'],
 })
